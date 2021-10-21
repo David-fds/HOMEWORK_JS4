3af1457f0d605172e5199dbaa39761b37d452e09
David Slusher <fdslusher@gmail.com>
2021-10-21 17:20:10 -0600

    dddd

--- classHw2.js
+++ classHw2.js
@@ -1,11 +1,15 @@
 var theData = require ("./google-objects.json");
-var theData2 = theData["items"];
-var arraySize = theData2.length;
+var theData2 = theData["items"];    //grab the "items" array separately
+var arraySize = theData2.length;    //get the number of items
 var countNum = 0;
 
-for (i = 0; i < arraySize; i++){
+for (var i = 0; i < arraySize; i++){
    if (theData2[i]["kind"] == "shopping#product"){
        countNum = countNum + 1;
+   }
+
+   if (theData2[i]["product"]["inventories"][0]["availability"] == "backorder"){
+        console.log(theData2[i]["product"]["title"] + " backordered");
    }
 }
 
